d9dd58ba017f3f28b0b87afaaf58e4b195f1582c
GabyOjVa <gabriela.ojedavalencia@gmail.com>
2023-11-03 14:37:48 -0500

    Correct tick labels for distribution plots

--- pcc_fig4f_anova_xsubs.m
+++ pcc_fig4f_anova_xsubs.m
@@ -25,7 +25,7 @@ for ss              = 1:length(all_subjects)
     all_out(ss)     = sub_out;
 end
 
-%% Correct P values for number of comparisons in each subject
+% %% Correct P values for number of comparisons in each subject
 % Fisrt load the limbic codes in both right and left hemis
 area_codes  = {[12123 53 54 12108 12109 12110 12106 12107 59 11123 17 18 11108 11109 11110 11106 11107 10]}; % all areas
 nr_subs     = length(all_subjects);
@@ -43,6 +43,8 @@ for ss = 1:nr_subs  % loop over subjects
     all_out(ss).hasdata     = NaN(size(all_out(ss).crp_out));
     all_out(ss).crp_p       = NaN(size(all_out(ss).crp_out));
     all_out(ss).a_prime     = NaN(size(all_out(ss).crp_out));
+    all_out(ss).tR          = NaN(size(all_out(ss).crp_out));
+    all_out(ss).Vsnr        = NaN(size(all_out(ss).crp_out));
     all_out(ss).cod         = NaN(size(all_out(ss).crp_out));
     all_out(ss).crp_p_adj   = NaN(size(all_out(ss).crp_out));
     all_out(ss).h           = NaN(size(all_out(ss).crp_out));
@@ -55,9 +57,11 @@ for ss = 1:nr_subs  % loop over subjects
             if ~isempty(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).data)   % we have CRPs
                 all_out(ss).hasdata(these_measured_sites(kk), these_stim_sites(ll)) = 1;            % is significant
                 all_out(ss).crp_p(these_measured_sites(kk), these_stim_sites(ll))   = all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_projs.p_value_tR;   % p-value
-                all_out(ss).a_prime(these_measured_sites(kk), these_stim_sites(ll)) = mean(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.al_p); % mean 
-                all_out(ss).cod(these_measured_sites(kk), these_stim_sites(ll))     = median(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.cod);    % CRP coefficent of determination
-                sig_timepoints = find(all_out(ss).tt>0.015 & all_out(ss).tt<=all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.tR);                 % response duration
+                all_out(ss).a_prime(these_measured_sites(kk), these_stim_sites(ll)) = mean(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.al_p);   % mean 
+                all_out(ss).tR(these_measured_sites(kk), these_stim_sites(ll))     = median(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.tR);    % CRP significant response duration
+                all_out(ss).Vsnr(these_measured_sites(kk), these_stim_sites(ll))     = median(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.Vsnr);% CRP signal to noise ratio
+                all_out(ss).cod(these_measured_sites(kk), these_stim_sites(ll))     = median(all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.cod);  % CRP coefficent of determination
+                sig_timepoints = find(all_out(ss).tt>0.015 & all_out(ss).tt<=all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.tR);                   % Signiticant timepoints
                 all_out(ss).avg_trace_tR(these_measured_sites(kk), these_stim_sites(ll),sig_timepoints) = all_out(ss).crp_out(these_measured_sites(kk), these_stim_sites(ll)).crp_parms.C; % al_p for tR
             else
                 all_out(ss).hasdata(these_measured_sites(kk), these_stim_sites(ll)) = 0;            % is not significant
@@ -72,7 +76,7 @@ for ss = 1:nr_subs  % loop over subjects
 end
 
 
-%% Load stim and measure areas across subjects
+% %% Load stim and measure areas across subjects
 % Sort limbic codes by hemisphere
 area_names      = {'Hipp','Amyg','PCC','ACC'};   
 area_codes_r    = {[12123 53],[54],[12108 12109 12110],[12106 12107]}; % right
@@ -83,7 +87,7 @@ out             = [];                   % prepare areaXarea matrix to concatinat
 
 subj_resp_total = zeros(nr_subs,1);     % stim-->measured pair for adjusted FDR
 
-t_win_norm      = [0.015 0.500];        % window for vector length normalization and plotting across subjects
+t_win_norm      = [0.015 0.500];        % window for vector length normalization
 
 for measure_ind = 1:length(area_names)  % loop over measured sites
     for stim_ind = 1:length(area_names) % now go through stimulated sites
@@ -133,6 +137,8 @@ for measure_ind = 1:length(area_names)  % loop over measured sites
                         
                         % save CRP parms for linear mixed-effects model
                         out(measure_ind,stim_ind).p(resp_counter, :)        = all_out(ss).crp_p_adj(these_measured_sites(kk), these_stim_sites(ll));
+                        out(measure_ind,stim_ind).Vsnr(resp_counter, :)     = all_out(ss).Vsnr(these_measured_sites(kk), these_stim_sites(ll));
+                        out(measure_ind,stim_ind).tR(resp_counter, :)       = all_out(ss).tR(these_measured_sites(kk), these_stim_sites(ll));
                         out(measure_ind,stim_ind).cod(resp_counter, :)      = all_out(ss).cod(these_measured_sites(kk), these_stim_sites(ll)); 
                         out(measure_ind,stim_ind).a_prime(resp_counter, :)  = all_out(ss).a_prime(these_measured_sites(kk), these_stim_sites(ll)); 
                     end
@@ -145,30 +151,33 @@ end
 %% General linear mixed effects model
 % Stim and measured ROI:
 % HC= 1; Amg= 2; PCC= 3; ACC= 4
-pcc_hc_cod              = out(3,1).cod;            % measured PCC (3) and stim HC (1)
-pcc_hc_stimgroup        = cell(size(pcc_hc_cod));  % hc stim gets code 1 
-pcc_hc_measgroup        = cell(size(pcc_hc_cod));  % pcc measure gets code 3
+
+%set CRP param to feed the model
+
+pcc_hc_crp              = out(3,1).cod;            % measured PCC (3) and stim HC (1)
+pcc_hc_stimgroup        = cell(size(pcc_hc_crp));  % hc stim gets code 1 
+pcc_hc_measgroup        = cell(size(pcc_hc_crp));  % pcc measure gets code 3
 [pcc_hc_stimgroup{:}]   = deal('HC');
 [pcc_hc_measgroup{:}]   = deal('PCC');
 sub_ind1                = out(3,1).subj_ind;        % HC-PCC connection 
 
-acc_hc_cod              = out(4,1).cod;             % measured ACC (4) and stim HC (1)
-acc_hc_stimgroup        = cell(size(acc_hc_cod));   % hc stim gets code 1
-acc_hc_measgroup        = cell(size(acc_hc_cod));   % acc measure gets code 4
+acc_hc_crp              = out(4,1).cod;             % measured ACC (4) and stim HC (1)
+acc_hc_stimgroup        = cell(size(acc_hc_crp));   % hc stim gets code 1
+acc_hc_measgroup        = cell(size(acc_hc_crp));   % acc measure gets code 4
 [acc_hc_stimgroup{:}]   = deal('HC');
 [acc_hc_measgroup{:}]   = deal('ACC');
 sub_ind2                = out(4,1).subj_ind;        % HC-ACC connection 
 
-pcc_amg_cod             = out(3,2).cod;             % measured PCC (3) and stim Amygdala (2)
-pcc_amg_stimgroup       = cell(size(pcc_amg_cod));  % amg stim gets code 2
-pcc_amg_measgroup       = cell(size(pcc_amg_cod));  % pcc measure gets code 3
+pcc_amg_crp             = out(3,2).cod;             % measured PCC (3) and stim Amygdala (2)
+pcc_amg_stimgroup       = cell(size(pcc_amg_crp));  % amg stim gets code 2
+pcc_amg_measgroup       = cell(size(pcc_amg_crp));  % pcc measure gets code 3
 [pcc_amg_stimgroup{:}]  = deal('AMG');
 [pcc_amg_measgroup{:}]  = deal('PCC');       
 sub_ind3                = out(3,2).subj_ind;        % Amg-PCC connection
 
-acc_amg_cod             = out(4,2).cod;             % measured ACC (4) and stim Amygdala (2)
-acc_amg_stimgroup       = cell(size(acc_amg_cod));  % amg stim gets code 2
-acc_amg_measgroup       = cell(size(acc_amg_cod));  % acc measure gets code 4
+acc_amg_crp             = out(4,2).cod;             % measured ACC (4) and stim Amygdala (2)
+acc_amg_stimgroup       = cell(size(acc_amg_crp));  % amg stim gets code 2
+acc_amg_measgroup       = cell(size(acc_amg_crp));  % acc measure gets code 4
 [acc_amg_stimgroup{:}]  = deal('AMG');
 [acc_amg_measgroup{:}]  = deal('ACC');
 sub_ind4                = out(4,2).subj_ind;        % Amg-ACC connection 
@@ -181,31 +190,33 @@ measure_site    = [pcc_hc_measgroup(:); acc_hc_measgroup(:); pcc_amg_measgroup(:
 
 tbl = table(y,categorical(stim_site),categorical(measure_site),categorical(sub_ind),...
     'VariableNames',{'ccep_val','stim_site','measure_site','sub_ind'});
-%% test different LME models for best fit
+%% Different LME models for best fit to test for interation of COD
 % Random intercept model with a fixed slope
-lme = fitlme(tbl,'ccep_val ~ 1 + stim_site*measure_site + (1|sub_ind)');
+lme = fitlme(tbl,'ccep_val ~ 1 + stim_site*measure_site + (1|sub_ind)'); 
 anova(lme);                             % perform F-test that all fixed-effects coefficients are zero
 anova(lme,'DFMethod','satterthwaite')   % get degrees of freedom w Satterthwaite method. Produces smaller denominator degrees of freedom and slightly larger p-values
 disp('Random intercept model with a fixed slope');
 
-% Random intercept and slope, with possible correlation between them
-lme2 = fitlme(tbl,'ccep_val ~ 1 + stim_site*measure_site + (1 + stim_site*measure_site|sub_ind)');
-anova(lme2);                            % perform F-test that all fixed-effects coefficients are zero
-anova(lme2,'DFMethod','satterthwaite')  % get degrees of freedom w Satterthwaite method. Produces smaller denominator degrees of freedom and slightly larger p-values
-disp('Random intercept and slope, with possible correlation between them');
-
-% Independent random effects terms for intercept and slope
-lme3 = fitlme(tbl,'ccep_val ~ 1 + stim_site*measure_site + (1|sub_ind) + (-1 + stim_site*measure_site|sub_ind)');
-anova(lme3);                            % perform F-test that all fixed-effects coefficients are zero
-anova(lme3,'DFMethod','satterthwaite')  % get degrees of freedom w Satterthwaite method. Produces smaller denominator deg
-disp('Independent random effects terms for intercept and slope')
 
 %% Compare LME models
-% %% Run a Theoretical Likelihood Ratio Test to choose the model to use
-% %% Check nested conditions between models with 1000 replications     
-compare(lme, lme2,'CheckNesting',true,'NSim',1000)  % compare smaller with larger model
-compare(lme3,lme2,'CheckNesting',true,'NSim',1000)  % compare complex models
-compare(lme, lme3,'CheckNesting',true,'NSim',1000)  % compare smaller and large model
+% % Other LME models where tested for a better fit
+% % Random intercept and slope, with possible correlation between them
+% lme2 = fitlme(tbl,'ccep_val ~ 1 + stim_site*measure_site + (1 + stim_site*measure_site|sub_ind)');
+% anova(lme2);                            % perform F-test that all fixed-effects coefficients are zero
+% anova(lme2,'DFMethod','satterthwaite')  % get degrees of freedom w Satterthwaite method. Produces smaller denominator degrees of freedom and slightly larger p-values
+% disp('Random intercept and slope, with possible correlation between them');
+% 
+% % Independent random effects terms for intercept and slope
+% lme3 = fitlme(tbl,'ccep_val ~ 1 + stim_site*measure_site + (1|sub_ind) + (-1 + stim_site*measure_site|sub_ind)');
+% anova(lme3);                            % perform F-test that all fixed-effects coefficients are zero
+% anova(lme3,'DFMethod','satterthwaite')  % get degrees of freedom w Satterthwaite method. Produces smaller denominator deg
+% disp('Independent random effects terms for intercept and slope')
+% 
+% % Run a Theoretical Likelihood Ratio Test to choose the model to use
+% % Check nested conditions between models with 1000 replications     
+% compare(lme, lme2,'CheckNesting',true,'NSim',1000)  % compare smaller with larger model
+% compare(lme3,lme2,'CheckNesting',true,'NSim',1000)  % compare complex models
+% compare(lme, lme3,'CheckNesting',true,'NSim',1000)  % compare smaller and large model
 
 %% Test LME further for stim ROI
 % now test only for PCC
@@ -214,7 +225,7 @@ y           = [pcc_hc_cod(:); pcc_amg_cod(:)];
 stim_site   = [pcc_hc_stimgroup(:); pcc_amg_stimgroup(:)]; % stim group
 tbl         = table(y,categorical(stim_site),categorical(sub_ind),'VariableNames',{'ccep_val','stim_site','sub_ind'});
 
-lme_pcc     = fitlme(tbl,'ccep_val ~ 1 + stim_site + (stim_site|sub_ind)');
+lme_pcc     = fitlme(tbl,'ccep_val ~ 1 + stim_site + (stim_site|sub_ind)','FitMethod','REML');
 anova(lme_pcc);                           % perform F-test that all fixed-effects coefficients are zero
 anova(lme_pcc,'DFMethod','satterthwaite') % degrees of freedom w Satterthwaite method
 disp('Stim site interaction in PCC ')
@@ -254,6 +265,6 @@ plotSpread(y, 'distributionIdx', plot_groups, 'xValues',1:4,'spreadWidth',.5, 'd
 plot([1:4],[median(y(plot_groups==1)) median(y(plot_groups==2)) median(y(plot_groups==3)) median(y(plot_groups==4))],'k.','MarkerSize',30);
 
 xlim([0 5]),ylim([-0.6 1.4]),ylabel('Explained Variance (R^2)')
-set(gca,'YTick',[0:.2:1],'XTick',[1:4],'XTickLabel',{'H-PCC','A-PCC','H-ACC','A-ACC'})
+set(gca,'YTick',[0:.2:1],'XTick',[1:4],'XTickLabel',{'H-ACC','A-ACC','H-PCC','A-PCC'})
 
 
